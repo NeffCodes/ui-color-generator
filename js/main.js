1c17bb8c32f535cfc0d77272ad1be588640bcba6
James Neff <james.m.neff@gmail.com>
2022-04-14 15:13:21 -0400

    Fetch data from colormind api

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-//Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value.toLowerCase()
-  const url = ``
+  const url = "http://colormind.io/api/";
+  const data = {
+    model : "ui",
+    input : ["N","N","N","N","N"] //[128,0,32] rgb value if custom
+    //structure: light shade, light accent, main, dark accent, dark shade
+  }
 
-  fetch(url)
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        })
       })
       .catch(err => {
           console.log(`error ${err}`)
